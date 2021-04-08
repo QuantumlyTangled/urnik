404692da566f72e3052da4bcef35cffb364aa3f8
QuantumlyTangled <ethoarx@gmail.com>
2021-04-08 20:12:52 +0200

    feat(testing): temp function call

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { getSchoolInfo } from 'utils/SchoolID';
 
 export default function Home() {
+	useState(async () => {
+		await getSchoolInfo('https://www.easistent.com/urniki/9dbc59a89b732fdf68e15a06c89f97444384f74d/');
+	});
+
 	return (
-		<></>
+		<>
+			<div>hi</div>
+			<div>hello</div>
+		</>
 	);
 }
